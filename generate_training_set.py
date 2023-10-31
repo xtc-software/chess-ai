b0130635826ef4c3f4dcc725320b878557c3339a
anuraag <anuraag.warudkar@gmail.com>
2023-10-31 00:46:42 -0400

    final engine config

--- generate_training_set.py
+++ generate_training_set.py
@@ -7,20 +7,28 @@ import chess.engine
 
 time_limit = chess.engine.Limit(time=0.05)
 stockfish_path = "/home/boop/stockfish/stockfish-ubuntu-x86-64-avx2"
-depth_limit = 20
+depth_limit = 10
+engine_config = {
+    "Threads": 4,  # Adjust to the number of CPU cores available
+    "Hash": 4096,  # Adjust based on available RAM (in MB)
+}
+
 
 def get_dataset(num_samples=None):
     X, Y = [], []
     gn = 0
     # trimdata.pgn
     pgn = open("./data/trimdata50k.pgn")
-    while 1:
-        game = chess.pgn.read_game(pgn)
-        if game is None:
-            break
+    with chess.engine.SimpleEngine.popen_uci(stockfish_path) as engine:
+        for key, value in engine_config.items():
+            engine.configure({key: value})
+        
+        while 1:
+            game = chess.pgn.read_game(pgn)
+            if game is None:
+                break
 
-        board = game.board()
-        with chess.engine.SimpleEngine.popen_uci(stockfish_path) as engine:
+            board = game.board()
             for _, move in enumerate(game.mainline_moves()):
                 board.push(move)
                 info = engine.analyse(board, limit=chess.engine.Limit(depth=depth_limit))
@@ -29,11 +37,11 @@ def get_dataset(num_samples=None):
                 Y.append(info["score"].relative.score() / 100)
                 
                 print(Y[-1]) #! need to update with eval after move
-        
-        print("parsing game %d, got %d examples" % (gn, len(X)), end='\r')
-        if num_samples is not None and len(X) > num_samples:
-            return X, Y
-        gn += 1
+            
+            print("parsing game %d, got %d examples" % (gn, len(X)), end='\r')
+            if num_samples is not None and len(X) > num_samples:
+                return X, Y
+            gn += 1
     X = np.array(X)
     Y = np.array(Y)
     return X, Y
